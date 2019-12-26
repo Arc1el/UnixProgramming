9979732ce587900da6d4d1e8a1fdc41de3980890
Arc1el <khm970514@gmail.com>
2019-12-26 18:07:45 +0900

    Rename score_management/output_prog.c to Application05_scoremanagement/output_prog.c

--- Application05_scoremanagement/output_prog.c
+++ Application05_scoremanagement/output_prog.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
         if(argc<2)
         {
-                printf("ÆÄÀÏ 1°³¿Í °°ÀÌ »ç¿ëÇÏ¼¼¿ä.\n»ç¿ë¿¹ : %s data.dat",argv[0]);
+                printf("íŒŒì¼ 1ê°œì™€ ê°™ì´ ì‚¬ìš©í•˜ì„¸ìš”.\nì‚¬ìš©ì˜ˆ : %s data.dat",argv[0]);
                 exit(1);
         }
         char *file;
